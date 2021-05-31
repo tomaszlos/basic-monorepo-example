86e8dd298d7a9bc397312aed553706270a452b3d
Tomasz Los <tomasz.los@rumblefish.pl>
2021-05-31 15:20:05 +0200

    [app2] example commit 3

--- apps/app2/src/App.js
+++ apps/app2/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Example edit 2
+          Example edit 3
         </p>
         <a
           className="App-link"
